458c506b8ccb20b24b83c0e832de7a88a81ee7d2
agcdam <alonsogomezcasanova@gmail.com>
2022-04-21 14:35:00 +0200

    Implementacion interfaz WinUI 3
    Comienzo de la implementacion de la interfaz con WinUI 3 en c++/winrt y xaml

---
+++ PdfVysorWinUI/MainWindow.xaml.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#include <winrt/Microsoft.UI.Interop.h>
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::PdfVysorWinUI::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    void winrt::PdfVysorWinUI::implementation::MainWindow::nav_Loaded(winrt::IInspectable const& sender, winrt::RoutedEventArgs const& e)
+    {
+
+    }
+
+
+    void winrt::PdfVysorWinUI::implementation::MainWindow::nav_ItemInvoked(winrt::NavigationView const& sender, winrt::NavigationViewItemInvokedEventArgs const& args)
+    {
+
+    }
+
+    void winrt::PdfVysorWinUI::implementation::MainWindow::Button_Click(winrt::IInspectable const& sender, winrt::RoutedEventArgs const& e)
+    {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PdfVysorWinUI/MainWindow.xaml.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+
+namespace winrt::PdfVysorWinUI::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+        void nav_Loaded(winrt::IInspectable const& sender, winrt::RoutedEventArgs const& e);
+        void nav_ItemInvoked(winrt::NavigationView const& sender, winrt::NavigationViewItemInvokedEventArgs const& args);
+        void Button_Click(winrt::IInspectable const& sender, winrt::RoutedEventArgs const& e);
+    };
+}
+
+namespace winrt::PdfVysorWinUI::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
